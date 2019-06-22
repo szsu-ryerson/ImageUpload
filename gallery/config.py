6889253d07578e083758aad6f45bf1607f8cc28f
szsu-ryerson <50525541+szsu-ryerson@users.noreply.github.com>
2019-06-22 11:22:56 -0400

    Update config.py

--- gallery/config.py
+++ gallery/config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-	SQLALCHEMY_DATABASE_URI="postgres://rcezaasgarfdnn:0ded998f6a193b89f391621e9fe1fc51b3d3c7a3db8b06b885f391af60166a6a@ec2-54-163-230-199.compute-1.amazonaws.com:5432/d2pvkehpsl7umo"
+	SQLALCHEMY_DATABASE_URI=
 	SQLALCHEMY_TRACK_MODIFICATIONS=False
-	S3_BUCKET="ccps530"
-	S3_SECRET="mr+O9KeC+zcT5mQEbDFhZcghj/ijCmCxWrnwG6x+"
-	S3_KEY="AKIAVQNMIRYRPSUIL7FH"
-	S3_LOCATION='https://ccps530.s3.ca-central-1.amazonaws.com'
+	S3_BUCKET=
+	S3_SECRET=
+	S3_KEY=
+	S3_LOCATION=
 	ALLOWED_EXTENSIONS=set(['txt','pdf','png','jpg','jpeg','gif'])
-	SECRET_KEY="SPFTIjokdkfjekls=esitd"
+	SECRET_KEY=
 
 	
